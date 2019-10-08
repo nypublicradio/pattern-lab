80e7892d4ecf267a985b0013ed55e4c948a13092
Terrance Pitre <tpitre@southleft.com>
2019-10-08 16:24:35 -0500

    Gallery bug in PL and beginning of style guide design (#77)
    * Adding welcome cover.
    
    * Adding color swatches.
    
    * Adjusting grid break.
    
    * Adding arrow to welcome page.
    
    * Adding type styles.
    
    * Adjusting article heaight when gallery is active. CMS-670

--- source/js/script.js
+++ source/js/script.js
@@ -246,4 +246,18 @@
     toggle(mql);
   })();
 
+  // Smooth scrolling on anchor clicks
+  $('.js-smooth-scroll').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
+
 })(jQuery);
